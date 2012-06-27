d1a1c31facf52b29110f9bcbdc92f7e12db6851c
brianhone@gmail.com <brianhone@gmail.com@0b191426-b8d4-d628-3dd7-a4cf867f7d4b>
2012-06-27 13:03:07 +0000

    fixed call to data extraction

--- savemat.py
+++ savemat.py
@@ -2,110 +2,11 @@
 
 __author__="Brian Hone"
 
-import sys, os, string, time, pprint
+import sys, os, string
 import numpy as np
 import scipy.io as sio
 import gdb
-
-def get_data( args ):
-    data = {}
-    I = 1j
-    for arg in args:
-        # If we're dealing with sub-structures, descend down
-        if arg.find('.') >=0 :
-            arg_split = arg.split( '.' )
-            x = gdb.selected_frame().read_var(arg_split[0])
-            for i in arg_split[1:]:
-                x = x[i]
-        # Else, just get the data
-        else:
-            print arg
-            try: ## try a stack variable
-                x = gdb.selected_frame().read_var(arg)
-            except: ## try a class member
-                x = gdb.selected_frame().read_var("this").dereference()[arg]
-        x_str = str(x)
-        ########################################
-        # BOOST VECTOR
-        ########################################
-        if x_str.find( "boost" ) >= 0:
-            ptr = x['data_']['data_']
-            size = x['data_']['size_']
-            vals = []
-            ## NOT COMPLEX
-            if x_str.find( 'std::complex' ) < 0:
-                for i in range( size ):
-                    loc = ptr + i
-                    vals.append( eval( str(loc.dereference() )) )
-                u = np.array( vals )
-                data[ arg ] = u
-            ## COMPLEX
-            else:
-                for i in range( size ):
-                    loc = ptr + i
-                    vals.append( eval( str(loc.dereference()['_M_value'])) )
-                u = np.array( vals ) 
-                data[ arg ] = u
-        ########################################
-        # STL VECTOR
-        ########################################
-        elif x_str.find( "std::vector" ) >= 0:
-            ptr = x['_M_impl']['_M_start']
-            end = x['_M_impl']['_M_finish']
-            vals = []
-            ## COMPLEX
-            if str(x.type).find( 'std::complex' ) >= 0:
-                while ptr != end:
-                    vals.append( eval( str( ptr.dereference()['_M_value'] ) ) )
-                    ptr = ptr + 1
-                u = np.array(vals)
-                data[ arg ] = u
-            ## Not Complex
-            else:
-                while ptr != end:
-                    vals.append( eval( str( ptr.dereference() ) ) )
-                    ptr = ptr + 1
-                u = np.array(vals)
-                data[ arg ] = u
-        ########################################
-        # Eigen
-        ########################################
-        elif x_str.find( "Eigen::Array" ) >= 0:
-            #
-            # NOTE: This only works for dynamic Eigen::Array 
-            #
-            if x_str.find("std::complex") >= 0:
-                ptr = x['m_storage']['m_data']
-                end = x['m_storage']['m_rows']
-                vals = []
-                for i in range(end):
-                    vals.append( eval( str( ptr.dereference()['_M_value'] ) ) ) 
-                    ptr = ptr + 1
-                u = np.array( vals ) 
-                data[ arg ] = u
-            else:
-                ptr = x['m_storage']['m_data']
-                end = x['m_storage']['m_rows']
-                vals = []
-                for i in range(end):
-                    vals.append( ptr.dereference() ) 
-                    ptr = ptr + 1
-                u = np.array( vals ) 
-                data[ arg ] = u
-
-        ########################################
-        # Unknown, try parsing the string
-        ########################################
-        else:
-            brace_pos = x_str.find('{') 
-            brace_pos2 = x_str.find('}')
-            x_str = '[ %s ]' % x_str[ brace_pos+1:brace_pos2]
-            s = eval( '%s' % x_str )
-            u = np.array( s ) 
-            data[ arg ] = u
-
-    return data
-# end get_data
+from gp_data_extractor import *
 
 class MatSaver( gdb.Command ):
     def __init__( self ):
@@ -114,8 +15,12 @@ class MatSaver( gdb.Command ):
     def invoke( self, arg, from_tty ):
         args = string.split( arg )
         fname = args[0]
-        data = get_data( args[1:] )
-        for key, val in data.items():
+        raw_data = gp_get_data( args[1:] )
+        data = {}
+        for i in range( len( raw_data ) ):
+            key = args[i+1]
+            val = raw_data[i]
+            data[ key ] = val
             print "Saving %s" % key   
         sio.savemat( fname, data )
 # end class Plotter
